86472e5d7d733239778d6b4d9d4660b5bf91fb67
darek122 <dark.wader122@gmail.com>
2018-07-19 15:03:18 +0200

    COs zrobone

--- src/main/java/pl/szymondarek/Starter.java
+++ src/main/java/pl/szymondarek/Starter.java
@@ -1,5 +1,7 @@
 package pl.szymondarek;
 
 public class Starter {
+    public static void main(String[] args) {
 
+    }
 }
--- src/main/java/pl/szymondarek/views/MainView.java
+++ src/main/java/pl/szymondarek/views/MainView.java
@@ -2,6 +2,12 @@ package pl.szymondarek.views;
 
 public class MainView {
 
+public void showLogRegisterMenu(){
+    System.out.println("Witaj urzytkowaniku");
+    System.out.println("Masz koto podaj 1");
+    System.out.println("Chcesz się zarejestrować podaj 2");
+}
+
 
 
     }
